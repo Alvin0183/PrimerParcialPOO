a68f094ae5b2c0097aa6db9a4af8215d7c0df149
Alvin0183 <alvinmartinez666@outlook.com>
2021-09-13 16:52:44 -0600

    Actualizacion

---
+++ Parcia 1 POO/Parcia 1 POO/Entidades/ClsUsusario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcia_1_POO.Negocio
+{
+    class Clsusario
+    {
+
+
+
+       {public string ACCESO(Clsusario clsusuario)
+        {
+            Clsusario clsusuario = new Clsusario();
+           
+            clsusuario.Nombre1 = "Alvin";
+            clsusuario.Contraseña1 = 123;
+
+            if (clsusuario.Nombre1 == "Alvin" || clsusuario.Contraseña1 == 123)
+            {
+                return "INICIO DE  SESIÓN CORRECTO PUEDES CONTINUAR CON EL PROCESO DE VENTA";
+
+
+            }
+            else
+            {
+                return "Nombre de usuario incorrecto";
+
+            }
+
+
+        }
+
+            Clsusario usuario = new Clsusario();
+            Nclusuario nclsusuario = new Nclusuario();
+         
+
+        public string Nombre1 { get; private set; }
+        public object Contraseña1 { get; private set; }
+
+        private class Nclusuario
+        {
+        }
+    }
+
+}
