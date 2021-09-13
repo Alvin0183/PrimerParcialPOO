387cfb1a530cc30fc5ef0a062fdf515eaa4ab60c
Alvin0183 <alvinmartinez666@outlook.com>
2021-09-13 15:36:26 -0600

    V1

---
+++ Parcia 1 POO/Parcia 1 POO/Entidades/Ususario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcia_1_POO.Entidades
+{
+    class Ususario
+    {
+        private string Nombre;
+        private int Contraseña;
+
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public int Contraseña1 { get => Contraseña; set => Contraseña = value; }
+    }
+}
